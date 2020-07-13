f5bb7dc036e6eea720630d2d89e3c46067ecc902
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2020-07-13 21:50:30 +0200

    Set Colors of Tabs

--- covid-19-tracker-react/src/components/TabComponent.js
+++ covid-19-tracker-react/src/components/TabComponent.js
@@ -3,6 +3,44 @@ import React, {Component} from 'react';
 class Tab extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            colors: {
+                allCases: '#1d2c4d',
+                activeCases : '#FF0000',
+                recoveredCases: '#25b840',
+                deathsCases: '#020d1f'
+            },
+            tabRefs: {
+                allCasesRef : React.createRef(),//cases
+                activeCasesRef: React.createRef(),//active
+                recoveredCasesRef: React.createRef(),//recovered
+                deathsCasesRef: React.createRef()//deaths
+            },
+            caseNumbers: {
+                allCases: this.props.worldData.cases, 
+                activeCases: this.props.worldData.active,
+                recovered: this.props.worldData.recovered,
+                deaths: this.props.worldData.deaths
+            }
+        }
+    }
+
+    componentDidMount(){
+        this.setCardValues();
+    }
+
+    setCardValues = () => {
+        var colors = Object.values(this.state.colors);
+        var tabRefs = Object.values(this.state.tabRefs);
+        var caseNumbers = Object.values(this.state.caseNumbers);
+        for(var i=0; i < colors.length; i++){
+            var html = `
+                <div style="color:${colors[i]}" >
+                    ${this.setCommas(caseNumbers[i])}
+                </div>
+            `;
+            tabRefs[i].current.innerHTML = html;
+        }
     }
 
     setCommas(number){
@@ -16,7 +54,7 @@ class Tab extends Component{
                     <div class="card" id="total-cases-card">
                         <div class="card-body">
                             <h5 class="card-title">Total Cases</h5>
-                            <h6 class="card-subtitle mb-2 text-muted" id="total-cases">{this.setCommas(this.props.worldData.cases)}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted" ref={this.state.tabRefs.allCasesRef}></h6>
                         </div>
                     </div>
                 </div>
@@ -24,7 +62,7 @@ class Tab extends Component{
                     <div class="card" id="active-cases-card">
                         <div class="card-body">
                             <h5 class="card-title">Active Cases</h5>
-                            <h6 class="card-subtitle mb-2 text-muted" id="active-cases">{this.setCommas(this.props.worldData.active)}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted" ref={this.state.tabRefs.activeCasesRef}></h6>
                         </div>
                     </div>
                 </div>
@@ -32,7 +70,7 @@ class Tab extends Component{
                     <div class="card" id="recovered-cases-card">
                         <div class="card-body">
                             <h5 class="card-title">Recovered</h5>
-                            <h6 class="card-subtitle mb-2 text-muted " id="recovered-cases">{this.setCommas(this.props.worldData.recovered)}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted " ref={this.state.tabRefs.recoveredCasesRef}></h6>
                         </div>
                     </div>
                 </div>
@@ -40,7 +78,7 @@ class Tab extends Component{
                     <div class="card" id="death-cases-card">
                         <div class="card-body">
                             <h5 class="card-title">Deaths</h5>
-                            <h6 class="card-subtitle mb-2 text-muted " id="death-cases">{this.setCommas(this.props.worldData.deaths)}</h6>
+                            <h6 class="card-subtitle mb-2 text-muted " ref={this.state.tabRefs.deathsCasesRef}></h6>
                         </div>
                     </div>
                 </div>
